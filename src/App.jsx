ae33e196acf81faffc8c360d48721889f705edc5
Natalya Baranova <byasha29400@gmail.com>
2026-05-23 20:29:37 +0300

    add state_task3

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
-import ParentComponent from "./ParentComponent";
-import ChildComponent from "./ChildComponent";
+import ProfileComponent from "./ProfileComponent";
+import TaskComponent from "./TaskComponent";
+import CartComponent from "./CartComponent";
 
 function App() {
   return (
     <>
-    <ParentComponent />
+    <ProfileComponent />
+    <TaskComponent />
+    <CartComponent />
     </>
   )
 }
